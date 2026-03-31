972b091e1ecda64c67066a9efad5e7c78dc684fe
lyhamissi <lyhamissi@gmail.com>
2026-03-31 15:04:34 +0200

    final project

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -22,6 +22,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 const allowedOrigins = [
   "http://localhost:5173",
+  "https://umuti.vercel.app",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
